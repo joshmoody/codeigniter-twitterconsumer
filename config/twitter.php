68ea2fd6164fd49883adcd490bd50f345bf8cbaa
Josh Moody <josh@moody-laptop.local>
2013-03-20 22:55:15 -0500

    Additional config items if producing RSS

--- config/twitter.php
+++ config/twitter.php
@@ -2,3 +2,8 @@
 
 $config['consumer_key']			= 'YOUR CONSUMER KEY';
 $config['consumer_secret']		= 'YOUR CONSUMER SECRET';
+
+// If producing RSS
+$config['feed_title']			= '@joshmoody';
+$config['feed_url']				= 'http://twitter.com/joshmoody';
+$config['feed_description']		= 'Feeds from @joshmoody on Twitter';
